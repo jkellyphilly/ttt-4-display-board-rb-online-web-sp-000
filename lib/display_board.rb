0e112448d110a05e071984da8b40eb1d5c3e70a2
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-18 01:50:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(current_board)
+def display_board(current_board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
   puts " #{current_board[0]} | #{current_board[1]}  | #{current_board[2]}  "
   puts "-----------"
   puts " #{current_board[3]}  | #{current_board[4]}  | #{current_board[5]}  "
